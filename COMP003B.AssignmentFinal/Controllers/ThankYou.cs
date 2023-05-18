0c492fc58fb85f03acfa8070456ede19dab40269
Jovani Benavides <xavibenavides@yahoo.com>
2023-05-17 19:23:33 -0700

    ll

---
+++ COMP003B.AssignmentFinal/Controllers/ThankYou.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.AssignmentFinal.Controllers
+{
+    public class ThankYou : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
