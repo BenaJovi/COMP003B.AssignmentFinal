4d9e2f27c1830ca104b950857ad4cf98877810a7
BenaJovi <xavibenavides@yahoo.com>
2023-05-19 20:33:20 -0700

    Two Views Added

--- COMP003B.AssignmentFinal/Models/Goal.cs
+++ COMP003B.AssignmentFinal/Models/Goal.cs
@@ -1,6 +1,25 @@
-﻿namespace COMP003B.AssignmentFinal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.AssignmentFinal.Models
 {
     public class Goal
     {
+        public int GoalId { get; set; }
+
+        [Required]
+        public string GoalWeight { get; set; }
+        [Required]
+        public string GoalHealth { get; set; }
+        [Required]
+        public string  GoalBenchPress { get; set; }
+        [Required]
+        public string GoalSquat { get; set; }
+        [Required]
+        public string GoalArmCurl { get; set; }
+        [Required]
+
+        public string GoalMileTime { get; set; }
+        [Required]
+        public string GoalStatus { get; set; }
     }
 }
--- COMP003B.AssignmentFinal/Models/Profile.cs
+++ COMP003B.AssignmentFinal/Models/Profile.cs
@@ -1,6 +1,17 @@
-﻿namespace COMP003B.AssignmentFinal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.AssignmentFinal.Models
 {
     public class Profile
     {
+        public int ProfileId { get; set; }
+
+        [Required]
+        public string ProfileHeight { get; set; }
+        [Required]
+        public string ProfileWeight { get; set; }
+        [Required]
+        public string ProfileGender { get; set; }
+     
     }
 }
